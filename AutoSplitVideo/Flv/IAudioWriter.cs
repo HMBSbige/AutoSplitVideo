b6461259809cd4116b1d3972a44aaae9823a0147
Bruce Wayne <admin@bige0.com>
2019-11-24 16:08:03 +0800

    drop obsolete projects

--- AutoSplitVideo/Flv/IAudioWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoSplitVideo.Flv
-{
-	internal interface IAudioWriter
-	{
-		void WriteChunk(byte[] chunk, uint timeStamp);
-		void Finish();
-		string Path { get; }
-	}
-}
--- AutoSplitVideo/Flv/IVideoWriter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoSplitVideo.Flv
-{
-	internal interface IVideoWriter
-	{
-		void WriteChunk(byte[] chunk, uint timeStamp, int frameType);
-		void Finish(FractionUInt32 averageFrameRate);
-		string Path { get; }
-	}
-}
